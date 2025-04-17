2a754fdcc8a6992715be94447523e09a5fc049b3
yduo-a11y <alexandand16@gmail.com>
2025-04-17 10:18:39 +0200

    Add files via upload

--- fighers.c
+++ fighers.c
@@ -30,7 +30,7 @@ void afficherCombattantsDisponibles(Combattant liste[], int taille) {// Affiche 
         printf("(%d) Nom : %s\n", i+1, liste[i].nom);
         printf("     PV max      : %d\n", liste[i].pvmax);
         printf("     PV actuels  : %d\n", liste[i].pvcourants);
-        printf("     Attaque     : %s (%d)\n", liste[i].nomatq, liste[i].attaque);
+        printf("     Attaque     : %s: (%d)\n", liste[i].nomatq, liste[i].attaque);
         printf("     Défense     : %d\n", liste[i].defense);
         printf("     Spécial     : %s\n", liste[i].competspe);
         printf("     Vitesse     : %d\n", liste[i].vitesse);
@@ -45,5 +45,60 @@ Combattant* miseaJourCombattants(liste[],int choixcombattant){
         liste[choixcombattant] = liste[choixcombattant+1];  
     }
 return liste;
-} 
+}
+
+int longueur_int(int n) {// Compter les chiffres d'un entier
+    int longueur = 0;
+    while (n > 0) {
+        n /= 10;
+        longueur++;
+    }
+
+    return longueur;
+}
+void allignement(int longueur){//fonction permettant d'lligner les colonnes
+    for(int i = 0; i <35-longueur; i++){
+        printf(" ");
+    }
+
+}
    
+void affichecombats(Combattant* equipe) {// Affiche les combats entre les deux équipes
+    printf("|");
+    int l = 0;// variable servant à l'allignementde des |
+    
+    for(int i = 0; i < 3; i++) {//affichage des noms des combattants
+        printf("   %s: ", equipe[i].nom);
+        l=strlen(equipe[i].nom);
+        aliignement(l);
+        printf("|");
+    }
+    printf("\n");
+    for(int a=0; a<3; a++){
+        printf("   pv:%d",equipe[a].pvcourants);
+        l=longueur_int(equipe[a].pvcourants);
+        allignement(l);
+        printf("|");
+    }    
+    for(int z=0; z<3; z++){
+        printf("   %s:%d ", equipe[z].nomatq, equipe[z].attaque);
+        l=strlen(equipe[z].nomatq)+longueur_int(equipe[z].attaque);
+        allignement(l);
+        printf("|");
+    }
+    for(int b=0; b<3; b++){
+        printf("   %s ", equipe[b].defense);
+        l=strlen(equipe[b].defense);
+        allignement(l);
+        printf("|");
+    }
+
+
+
+
+
+
+
+
+
+}   
--- fightersmain.c
+++ fightersmain.c
@@ -6,7 +6,13 @@ int main(){
     Combattant équipe1[3];
     Combattant équipe2[3];
     Combattant liste[12];
-    
+    int choix1;
+    int compteur1=1;
+    int compteur2=1;
+    int compteur3=0;
+    char nomEquipe1[51];
+    char nomEquipe2[51];
+    int nombredetour=1;
     FILE *f1 = fopen("combattants/mme enserre-main", "r");//ouverture de tous les fichiers des combattans
     if (f1 == NULL) {
         exit(1);
@@ -58,42 +64,44 @@ int main(){
     liste[10] = ConveCombattant(f11);
     liste[11] = ConveCombattant(f12);
     
+
+    printf("Joueur 1, choisissez un nom d'équipe :\n");
+    scanf(" %50[^\n]", nomEquipe1);  // lit toute la ligne 
+
+    printf("Joueur 2, choisissez un nom d'équipe :\n");
+    scanf(" %50[^\n]", nomEquipe2); 
     
-    
-    int choix1;
-    int compteur1=1;
-    int compteur2=1;
-    for(int i=0; i<12;i++){
+    for(int i=0; i<3;i++){
         do{
-            afficherCombattantsDisponibles(liste, 12-compteur);//afficher les combattants disponibles
+            afficherCombattantsDisponibles(liste, 12-compteur3);//afficher les combattants disponibles
             printf("Joueur1 choisissez votre combattant numéro %d en saisissant sont numéro",compteur1);
             scanf("%d", &choix1);
            
         }while(choix1<1 || choix1>12-compteur || );
+        équipe1[i] = liste[choix1-1];//affecter le combattant choisi à l'équipe 1
         miseaJourCombattants(liste, choix1);//mise à jour de la liste de combattants
         systeme("clear");//nettoyer l'écran
-        compteur2++;
+        compteur1++;
+        compteur3++;
         do{
-            afficherCombattantsDisponibles(liste, 12-compteur);
+            afficherCombattantsDisponibles(liste, 12-compteur3);
             printf("Joueur2 choisissez votre combattant numéro %d en saisissant sont numéro",compteur2);
             scanf("%d", &choix1);
            
         }while(choix1<1 || choix1>12-compteur || );
+        équipe2[i] = liste[choix1-1];
         miseaJourCombattants(liste, choix1);//mise à jour de la liste de combattants
         systeme("clear");
         compteur2++;
+        compteur3++;
 
     }
+    do{//déroulement et affichage du combat
+    printf("Tour numéro %d\n",nombredetour);
 
 
-
-
-
-
-
-
-
-
+        nombredetour++;
+    }while((equipe1[0].pvcourants>0 && equipe[1].pvcourants>0 && equipe1[2].pvcourants) && (equipe2[1].pvcourants>0 && equip2[2].pvcourants>0 && equipe2[2].pvcourants) );//condition de fin de combat
 
 
 
