13f12b7997c3fc7109f8d2ae8125119561e9c8d5
yduo-a11y <alexandand16@gmail.com>
2025-04-17 11:49:40 +0200

    Add files via upload

--- fighers.c
+++ fighers.c
@@ -10,7 +10,8 @@ Combattant ConveCombattant(FILE* fichier){
     fscanf(fichier, "%d\n", &c.attaque);          // attaque
     fscanf(fichier, "%f\n", &c.defense);          // defense
     fscanf(fichier, "%29[^\n]\n", c.competspe);   // competspe
-    fscanf(fichier, "%d\n", &c.vitesse);          // vitesse
+    fscanf(fichier, "%d\n", &c.vitesse);
+    c.vitessecourant=0;          // vitesse
     fscanf(fichier, "%f\n", &c.esquive);          // esquive
 
     // Valeurs par défaut pour les dégâts spéciaux
@@ -18,7 +19,7 @@ Combattant ConveCombattant(FILE* fichier){
     c.degaspe2 = 0;
     c.degaspe3 = 0;
 
-    fclose(fichier);
+    ;
     return c;
 }
 
@@ -28,13 +29,13 @@ void afficherCombattantsDisponibles(Combattant liste[], int taille) {// Affiche 
     printf("\n\n");
     for (int i = 0; i < taille; i++) {
         printf("(%d) Nom : %s\n", i+1, liste[i].nom);
-        printf("     PV max      : %d\n", liste[i].pvmax);
-        printf("     PV actuels  : %d\n", liste[i].pvcourants);
-        printf("     Attaque     : %s: (%d)\n", liste[i].nomatq, liste[i].attaque);
-        printf("     Défense     : %d\n", liste[i].defense);
-        printf("     Spécial     : %s\n", liste[i].competspe);
-        printf("     Vitesse     : %d\n", liste[i].vitesse);
-        printf("     Esquive     : %d\n", liste[i].esquive);
+        printf("PV max: %d\n", liste[i].pvmax);
+        printf("Attaque: %s: (%d)\n", liste[i].nomatq, liste[i].attaque);
+        printf("Défense: %f\n", liste[i].defense);
+        printf("Compétence spéciale: %s\n", liste[i].competspe.nomspe);
+        printf("Description: %s %s", liste[i].competspe.description1,liste[i].competspe.description2);
+        printf("Vitesse: %d\n", liste[i].vitesse);
+        printf("Esquive: %f\n", liste[i].esquive);
         printf("----------------------------------------\n");
         printf("\n");
     }
@@ -57,45 +58,105 @@ int longueur_int(int n) {// Compter les chiffres d'un entier
     return longueur;
 }
 void allignement(int longueur){//fonction permettant d'lligner les colonnes
-    for(int i = 0; i <35-longueur; i++){
+    for(int i = 0; i <50-longueur; i++){
         printf(" ");
     }
-
+    printf("|");
 }
    
+void affichevitesse(int vitesse){
+    int a=vitesse/10;
+    printf("(");
+    for(int i=0; i<a; i++){
+        printf(">");
+    }
+    for(int b = 0; b <10-longueur; b++){
+        printf(" ");
+    }    
+    printf(")");
+}
+
+
+
+
 void affichecombats(Combattant* equipe) {// Affiche les combats entre les deux équipes
+    for(int t=0; t<(53*3+4); t++){
+        printf("_")   
+    }
+    printf("\n");
     printf("|");
     int l = 0;// variable servant à l'allignementde des |
     
     for(int i = 0; i < 3; i++) {//affichage des noms des combattants
-        printf("   %s: ", equipe[i].nom);
+        printf("   %s:", equipe[i].nom);
         l=strlen(equipe[i].nom);
-        aliignement(l);
-        printf("|");
+        allignement(l+1);    
     }
     printf("\n");
-    for(int a=0; a<3; a++){
+    printf("|");
+    for(int e=0; e<3; e++){
+        affichevitesse(equipe[e].vitessecourant);//affichage de la barre de vitesse
+        l=12;
+        allignement(l);    
+    }
+    printf("\n");
+    printf("|");
+    for(int r=0; r<3; r++){
+        l=-3;
+        allignement(l);    
+    }
+    printf("\n");
+    printf("|");
+    for(int a=0; a<3; a++){// affichage des pv 
         printf("   pv:%d",equipe[a].pvcourants);
         l=longueur_int(equipe[a].pvcourants);
-        allignement(l);
-        printf("|");
-    }    
-    for(int z=0; z<3; z++){
-        printf("   %s:%d ", equipe[z].nomatq, equipe[z].attaque);
+        allignement(l+3);    
+    }
+    printf("\n");
+    printf("|");    
+    for(int z=0; z<3; z++){// affichage des attaques
+        printf("   %s:%d", equipe[z].nomatq, equipe[z].attaque);
         l=strlen(equipe[z].nomatq)+longueur_int(equipe[z].attaque);
-        allignement(l);
-        printf("|");
+        allignement(l+1);   
     }
-    for(int b=0; b<3; b++){
-        printf("   %s ", equipe[b].defense);
+    printf("\n");
+    printf("|");
+    for(int b=0; b<3; b++){// affichage des défenses
+        printf("   défense:%d", equipe[b].defense);
         l=strlen(equipe[b].defense);
-        allignement(l);
-        printf("|");
+        allignement(l+8);    
     }
-
-
-
-
+    printf("\n");
+    printf("|");
+    for(int c=0; c<3; c++){// affichage des compétences spéciales
+        printf("   spé:%s", equipe[c].competspe.nomspe);
+        l=strlen(equipe[c].competspe.nomspe);
+        allignement(l+4);     
+    }
+    printf("\n");   
+    printf("|");
+    for(int d=0; d<3; d++){// affichage des descriptions
+        printf("   %s", equipe[d].competspe.description1);
+        l=strlen(equipe[d].competspe.description1);
+        allignement(l);   
+    }
+    printf("\n");   
+    printf("|");   
+    for(int f=0; f<3; f++){
+        printf("   %s", equipe[f].competspe.description2);
+        l=strlen(equipe[f].competspe.description2);
+        allignement(l);   
+    }
+    printf("\n");
+    printf("|");
+    for(int g=0; g<3; g++){
+        printf("   esquive:%d  ", equipe[g].esquive);
+        l=strlen(equipe[g].esquive);
+        allignement(l+8);   
+    }
+    
+    
+    
 
 
 
--- fightersmain.c
+++ fightersmain.c
@@ -15,40 +15,40 @@ int main(){
     int nombredetour=1;
     FILE *f1 = fopen("combattants/mme enserre-main", "r");//ouverture de tous les fichiers des combattans
     if (f1 == NULL) {
-        exit(1);
+        exit(1);}
     FILE *f2 = fopen("combattants/mme gros_t'es_qui ?", "r");
     if (f2 == NULL) {    
-        exit(2);  
+        exit(2); } 
     FILE *f3 = fopen("combattants/mme menu-bretin", "r");
     if (f3 == NULL) {    
-        exit(3);
+        exit(3);}
     FILE *f4 = fopen("combattants/mme picasso", "r");
     if (f4 == NULL) {   
-        exit(4);;
+        exit(4);}
     FILE *f5 = fopen("combattants/mr j’accuse_iel", "r");
     if (f5 == NULL) {  
-        exit(5);  
+        exit(5); } 
     FILE *f6 = fopen("combattants/mr abdelmoulaga", "r");
     if (f6 == NULL) {  
-        exit(6);   
+        exit(6);  } 
     FILE *f7 = fopen("combattants/mr arancini", "r");
     if (f7 == NULL) {    
-        exit(7);
+        exit(7);}
     FILE *f8 = fopen("combattants/mr bissap", "r");
     if (f8 == NULL) { 
         exit(8);  
     FILE *f9 = fopen("combattants/mr gay_prides", "r");
     if (f9 == NULL) { 
-        exit(9);
+        exit(9);}
     FILE *f10 = fopen("combattants/mr grognon", "r");
     if (f10 == NULL) {
-        exit(10);
+        exit(10);}
     FILE *f11 = fopen("combattants/mr par-dessus", "r");
     if (f11 == NULL) {
-        exit(11);  
+        exit(11); } 
     FILE *f12 = fopen("combattants/mr pelleteuse", "r");
     if (f12 == NULL) {
-        exit(12);  
+        exit(12); } 
     }
 
     liste[0] = ConveCombattant(f1);//affecter les combattants aux fichiers
@@ -77,7 +77,7 @@ int main(){
             printf("Joueur1 choisissez votre combattant numéro %d en saisissant sont numéro",compteur1);
             scanf("%d", &choix1);
            
-        }while(choix1<1 || choix1>12-compteur || );
+        }while(choix1<1 || choix1>12-compteur3 || );
         équipe1[i] = liste[choix1-1];//affecter le combattant choisi à l'équipe 1
         miseaJourCombattants(liste, choix1);//mise à jour de la liste de combattants
         systeme("clear");//nettoyer l'écran
@@ -101,7 +101,7 @@ int main(){
 
 
         nombredetour++;
-    }while((equipe1[0].pvcourants>0 && equipe[1].pvcourants>0 && equipe1[2].pvcourants) && (equipe2[1].pvcourants>0 && equip2[2].pvcourants>0 && equipe2[2].pvcourants) );//condition de fin de combat
+    }while((equipe1[0].pvcourants>0 && equipe[1].pvcourants>0 && equipe1[2].pvcourants) && (equipe2[1].pvcourants>0 && equip2[2].pvcourants>0 && equipe2[2].pvcourants>0) );//condition de fin de combat
 
 
 
